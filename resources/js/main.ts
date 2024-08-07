22db3801ed3eda396a5bdd327c251f3c7dc016a1
Daniel Schreurs <daniel.schreurs@hotmail.com>
2024-08-07 15:37:37 +0200

    :heavy_minus_sign: Removing swiper

--- resources/js/main.ts
+++ resources/js/main.ts
@@ -1,6 +1,5 @@
 import {Filter} from "./Filter";
 import {VideoController} from "./VideoController";
-import Swiper from 'swiper';
 import {settings} from "./settings";
 import {SearchForm} from "./SearchForm";
 import {GoUpBtn} from "./GoUpBtn";
@@ -10,16 +9,6 @@ document.documentElement.classList.add('js');
 const url = new URL(window.location.href);
 
 new VideoController();
-
-(new Swiper(".slider-section--news .slider-section__slider", settings.news)).init();
-(new Swiper(".categories__slider", settings.categories)).init();
-(new Swiper(".course-pictures", settings.course_pictures)).init();
-
-settings.courseSlider.selectors.forEach((selector) => {
-    if (document.querySelector(`${selector}`)) {
-        new Swiper(`${selector} .slider-section__slider`, settings.courseSlider.generateSwiperSettings(selector));
-    }
-});
 
 const filter = new Filter(settings.filter, url);
 const search = new SearchForm(settings.searchForm, url);
--- resources/js/settings.ts
+++ resources/js/settings.ts
@@ -1,174 +1,20 @@
-import {A11y, FreeMode, Keyboard, Navigation, Pagination} from 'swiper/modules';
-
 export const settings = {
     video: {
         videoSeekTime: 0.5,
         selector: 'video',
-    },
-    news: {
-        modules: [Navigation, Pagination, Keyboard, A11y],
-        slidesPerView: 1.1,
-        spaceBetween: 16,
-        loop: false,
-        slidesOffsetBefore: 16,
-        keyboard: {
-            enabled: true,
-        },
-        navigation: {
-            nextEl: '.slider-section--news .swiper-button-next',
-            prevEl: '.slider-section--news .swiper-button-prev',
-        },
-        pagination: {
-            el: ".slider-section--news .swiper-pagination",
-            clickable: true
-        },
-        breakpoints: {
-            480: {
-                slidesPerView: 2.2,
-                spaceBetween: 30
-            },
-            1200: {
-                slidesPerView: 4,
-                spaceBetween: 20
-            },
-            1900: {
-                slidesPerView: 4,
-                slidesOffsetBefore: 0,
-            }
-
-        },
-        a11y: {
-            enabled: true,
-            paginationBulletMessage: 'Aller à l’actualité {{index}}',
-            containerMessage: 'La liste de toutes les actualités',
-            prevSlideMessage: 'Actualité précédente',
-            nextSlideMessage: 'Actualité suivante',
-            firstSlideMessage: 'Première actualité',
-            lastSlideMessage: 'Dernière actualité',
-        }
-    },
-    courseSlider: {
-        selectors: ['.slider-section--course-si', '.slider-section--course-ss', '.slider-section--course-stc'],
-        generateSwiperSettings: (selector: string) => {
-            return {
-                modules: [Navigation, Pagination, Keyboard, A11y, FreeMode],
-                slidesPerView: 1.1,
-                spaceBetween: 20,
-                loop: false,
-                slidesOffsetBefore: 16,
-                freeMode: true,
-                navigation: {
-                    nextEl: `${selector} .swiper-button-next`,
-                    prevEl: `${selector} .swiper-button-prev`,
-                },
-                pagination: {
-                    clickable: true
-                },
-                breakpoints: {
-                    600: {
-                        slidesPerView: 2.2,
-                    },
-                    1200: {
-                        slidesPerView: 3.2,
-                    },
-                    1400: {
-                        slidesPerView: 4.2,
-                    },
-                    1900: {
-                        slidesPerView: 4.0,
-                    },
-                },
-                a11y: {
-                    enabled: true,
-                    paginationBulletMessage: 'Aller à la formation {{index}}',
-                    containerMessage: 'La liste de toutes les formations',
-                    prevSlideMessage: 'Formations précédente',
-                    nextSlideMessage: 'Formations suivante',
-                    firstSlideMessage: 'Première formation',
-                    lastSlideMessage: 'Dernière formation',
-                }
-            }
-        }
-    },
-
-    categories: {
-        modules: [Navigation, Pagination, Keyboard, A11y],
-        slidesPerView: 1.3,
-        spaceBetween: 16,
-        loop: false,
-        keyboard: {
-            enabled: true,
-        },
-        navigation: {
-            nextEl: '.categories .swiper-button-next',
-            prevEl: '.categories .swiper-button-prev',
-        },
-        pagination: {
-            el: ".categories .swiper-pagination",
-            clickable: true
-        },
-        breakpoints: {
-            480: {
-                slidesPerView: 3.2,
-                spaceBetween: 30
-            },
-            600: {
-                slidesPerView: 4.2,
-                spaceBetween: 40
-            },
-            1400: {
-                slidesPerView: 7,
-                spaceBetween: 20
-            }
-        },
-        a11y: {
-            enabled: true,
-            paginationBulletMessage: 'Aller à la catégorie {{index}}',
-            containerMessage: 'La liste de toutes les catégories de nos formations',
-            prevSlideMessage: 'Catégorie précédente',
-            nextSlideMessage: 'Catégorie suivante',
-            firstSlideMessage: 'Première catégorie',
-            lastSlideMessage: 'Dernière catégorie',
-        }
-    },
-    course_pictures: {
-        modules: [Navigation, Pagination, Keyboard, A11y],
-        slidesPerView: 4.3,
-        spaceBetween: 16,
-        loop: true,
-        keyboard: {
-            enabled: true,
-        },
-        navigation: {
-            nextEl: '.course-pictures .swiper-button-next',
-            prevEl: '.course-pictures .swiper-button-prev',
-        },
-        pagination: {
-            el: '.course-pictures .swiper-pagination',
-            clickable: true
-        },
-        a11y: {
-            enabled: true,
-            paginationBulletMessage: 'Aller à l’image {{index}}',
-            containerMessage: 'La liste de toutes les images de nos formations',
-            prevSlideMessage: 'Image précédente',
-            nextSlideMessage: 'Image suivante',
-            firstSlideMessage: 'Premièr image',
-            lastSlideMessage: 'Dernière image',
-        }
     },
     searchForm: {
         formSelector: '.search-form',
         inputSelector: '.search-form__input-container__search',
         templateSelector: '#keyboard-template',
         operatingSystemSelector: '#os',
-        itemsSelector: '.card--course',
+        itemsSelector: '.slider-section--course .slider__slide',
         hiddenClass: 'visually-hidden',
         showClass: 'card--show',
     },
     filter: {
         formSelector: '#filter-form',
-        itemSelector: '.card--course',
+        itemSelector: '.slider-section--course .slider__slide',
         showClass: 'card--show',
         hiddenClass: 'visually-hidden',
         categoryInputSelector: '.categories__slider input[type="checkbox"]',
